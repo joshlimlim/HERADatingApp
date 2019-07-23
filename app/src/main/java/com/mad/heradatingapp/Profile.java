9bbf6e0389dae818d0d4c56ee05b32985c5542b5
Joshua Lim <s10179502@connect.np.edu.sg>
2019-07-23 21:44:55 +0800

    More swipe card codes

---
+++ app/src/main/java/com/mad/heradatingapp/Profile.java
@@ -0,0 +1,40 @@
+package com.mad.heradatingapp;
+
+public class Profile {
+    private String name;
+    private String imageUrl;
+    private Integer age;
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}

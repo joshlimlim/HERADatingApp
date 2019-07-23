5d445001dc1c33f35dd0a3b3a0c0e1cbdda2b48f
Joshua Lim <s10179502@connect.np.edu.sg>
2019-07-23 22:54:16 +0800

    Register and Login event handlers created

--- app/src/main/java/com/mad/heradatingapp/MainActivity.java
+++ app/src/main/java/com/mad/heradatingapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mad.heradatingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Firebase connection succcess", Toast.LENGTH_LONG).show();
+    }
+
+    public void onSignup(View v){
+        Button btnSignup = (Button)v;
+        Intent in = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(in);
     }
 }
